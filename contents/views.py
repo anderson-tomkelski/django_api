f00e589b77ac9010e6d88a295d7bf42a212ac2b1
anderson-tomkelski <ander.tomkelski@gmail.com>
2020-01-13 15:31:00 -0300

    Created post and get for content

--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from contents.models import Content
+from contents.serializers import ContentSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class ContentList(APIView):
+    def get(self, request, format=None):
+        contents = Content.objects.all()
+        serializer = ContentSerializer(contents, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ContentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
